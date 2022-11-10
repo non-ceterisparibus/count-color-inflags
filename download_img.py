d8aff9ec979548765d19605eed6051909e2cbb45
non-ceterisparibus <duongthu1012@gmail.com>
2022-11-10 14:17:48 +0700

    test timeout error at request.get

--- download_img.py
+++ download_img.py
@@ -1,3 +1,4 @@
+from urllib.request import urlopen
 import requests
 import bs4
 import os
@@ -18,18 +19,25 @@ def get_image_url(soup):
 
 
 def download_and_save_image(image_url, img_name, cwd):
-    response = requests.get(f"{image_url}")
+    response = requests.get(f"{image_url}", stream=True,  verify=False, allow_redirects=True)
     file = open(f'{cwd}\\img\\{img_name}.png', 'wb')
     file.write(response.content)
     file.close()
 
+def download_and_save_image1(image_url, img_name, cwd):
+    response = requests.get(f"{image_url}", stream=True,  verify=False, allow_redirects=True)
+    file = open(f'{cwd}\\img\\{img_name}.png', 'wb')
+    file.write(response.content)
+    file.close()
 
+response = urlopen("https://www.example.com")
+body = response.read()
+>>> response.close()
 #test the functions
 if __name__ == '__main__':
 
-    html_soup = parse_page("https://en.wikipedia.org/wiki/File:Flag_of_Bangladesh.svg")
+    html_soup = parse_page("https://en.wikipedia.org/wiki/File:Flag_of_Zimbabwe.svg")
 
     image_url = get_image_url(html_soup)
-    print(image_url)
 
-    download_and_save_image(image_url, "Bangladesh", cwd)
+    download_and_save_image(image_url, "Zimbabwe", cwd)
