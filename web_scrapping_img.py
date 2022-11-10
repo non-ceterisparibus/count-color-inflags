c0f0d85c0ef7bbd9598b56f5b221d6d25b2c489c
non-ceterisparibus <duongthu1012@gmail.com>
2022-11-10 17:31:39 +0700

    Update web_scrapping_img.py

--- web_scrapping_img.py
+++ web_scrapping_img.py
@@ -5,9 +5,9 @@ import os
 from download_img import *
 
 cwd = os.getcwd()
+
 #send a request to wikipedia website with a list of all states
 url = 'https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags'
-nurl = 'https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations'
 wikiURL = 'https://en.wikipedia.org/'
 soup = parse_page(url)
 
@@ -18,6 +18,5 @@ for country in soup.find_all('div', class_='thumb'):
     soup_img = parse_page(fullImgLink)
     image_url = get_image_url(soup_img)
     download_and_save_image(f"{image_url}", country_name, cwd)
-
 
 print('Done')
